024b38c451bd334820fcd981da63929a08df2810
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-06-29 10:15:26 +0300

    add kotlin support

--- gradle_projects/multi-misc/apps/memcached/build.gradle.kts
+++ gradle_projects/multi-misc/apps/memcached/build.gradle.kts
@@ -1,9 +1,14 @@
 plugins {
     `java-library`
+    kotlin("jvm") version "1.8.22"
 }
 
 java {
     toolchain {
         languageVersion.set(JavaLanguageVersion.of(17))
     }
+}
+
+dependencies {
+    implementation(kotlin("stdlib"))
 }
