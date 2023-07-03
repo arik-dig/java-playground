c47461b0dcd4039e09ff907d5cc90a689197cae6
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-07-03 14:22:46 +0300

    bring memcached , junit and mockito

--- gradle_projects/multi-misc/apps/memcached/build.gradle.kts
+++ gradle_projects/multi-misc/apps/memcached/build.gradle.kts
@@ -9,6 +9,19 @@ java {
     }
 }
 
+val junitJupiterVersion = "5.9.2"
+val mockitoVersion = "5.2.0"
+
 dependencies {
     implementation(kotlin("stdlib"))
+
+    implementation("net.spy:spymemcached:2.12.3")
+
+    testImplementation("org.junit.jupiter:junit-jupiter-api:$junitJupiterVersion")
+    testRuntimeOnly   ("org.junit.jupiter:junit-jupiter-engine:$junitJupiterVersion")
+    testImplementation("org.mockito:mockito-junit-jupiter:$mockitoVersion")
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
