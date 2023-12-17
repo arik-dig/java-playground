ccd1f048104149b9a038a8c28e3c7057686d8503
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2023-12-17 14:57:18 +0200

    add junit 4

---
+++ maven_projects/maven_multi_misc/apps/service-k-jetty/src/test/java/com/digma/arik/servicek/JamminTest.java
@@ -0,0 +1,25 @@
+package com.digma.arik.servicek;
+
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class JamminTest {
+
+	@Test
+	public void xyzTest() {
+
+	}
+
+
+	@Test
+	public void failOnPurpose() {
+		assertEquals("a", "b");
+	}
+
+	@Test
+	public void abcTest() {
+
+	}
+
+}
