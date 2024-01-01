e610215e1a2d263b9a34dc2fc4bb5fd5a88b09a1
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2024-01-01 09:32:05 +0200

    add unexpectedErrorOnPurpose

--- maven_projects/maven_multi_misc/apps/service-k-jetty/src/test/java/com/digma/arik/servicek/JamminTest.java
+++ maven_projects/maven_multi_misc/apps/service-k-jetty/src/test/java/com/digma/arik/servicek/JamminTest.java
@@ -6,20 +6,25 @@ import static junit.framework.TestCase.assertEquals;
 
 public class JamminTest {
 
-	@Test
-	public void xyzTest() {
+    @Test
+    public void xyzTest() {
 
-	}
+    }
 
 
-	@Test
-	public void failOnPurpose() {
-		assertEquals("a", "b");
-	}
+    @Test
+    public void failOnPurpose() {
+        assertEquals("a", "b");
+    }
 
-	@Test
-	public void abcTest() {
+    @Test
+    public void abcTest() {
 
-	}
+    }
+
+    @Test
+    public void unexpectedErrorOnPurpose() {
+        throw new RuntimeException("so called unexpected");
+    }
 
 }
