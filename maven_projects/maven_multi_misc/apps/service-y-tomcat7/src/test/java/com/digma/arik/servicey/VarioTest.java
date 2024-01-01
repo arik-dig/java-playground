81423ebf0503a0e46af1ea13b1fa627788c57c31
Arik Sher <104715391+arik-dig@users.noreply.github.com>
2024-01-01 09:36:09 +0200

    add unexpectedErrorOnPurpose

--- maven_projects/maven_multi_misc/apps/service-y-tomcat7/src/test/java/com/digma/arik/servicey/VarioTest.java
+++ maven_projects/maven_multi_misc/apps/service-y-tomcat7/src/test/java/com/digma/arik/servicey/VarioTest.java
@@ -6,20 +6,24 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class VarioTest {
 
-	@Test
-	public void xyzTest() {
+    @Test
+    public void xyzTest() {
 
-	}
+    }
 
+    @Test
+    public void failOnPurpose() {
+        assertEquals("a", "b");
+    }
 
-	@Test
-	public void failOnPurpose() {
-		assertEquals("a", "b");
-	}
+    @Test
+    public void abcTest() {
 
-	@Test
-	public void abcTest() {
+    }
 
-	}
+    @Test
+    public void unexpectedErrorOnPurpose() {
+        throw new RuntimeException("so called unexpected");
+    }
 
 }
